2b5c3d0fe14ceb2a41001ebc3d9a19540caaeb70
denniskaboro <denniskaboro@gmail.com>
2021-06-04 19:14:31 +0300

    prediction part

--- Data_analysis/data_project.py
+++ Data_analysis/data_project.py
@@ -162,31 +162,32 @@ st.plotly_chart(col6)
 
 #predict for future incidents
 
+
 #get rid of the other columns
-df = df[['DATE', 'Downtime (hrs)']]
-df
+df2 = df[['DATE', 'Downtime (hrs)']]
+df2
 
 #Now, we can convert the “Downtime (hrs)” data type to float
-df = df.astype({"Downtime (hrs)": float})
-df["DATE"] = pd.to_datetime(df.DATE, format="%m/%d/%Y")
-df.dtypes
+df2 = df2.astype({"Downtime (hrs)": float})
+df2["DATE"] = pd.to_datetime(df2.DATE, format="%m/%d/%Y")
+df2.dtypes
 
 #Index Column
 df.index = df['DATE']
 
 #plot  the data on a graph
-plt.plot(df["Downtime (hrs)"],label='Downtime tred')
+plt.plot(df['Downtime (hrs)'],label='Downtime tred')
 
 
 #Data preparation
-df = df.sort_index(ascending=True,axis=0)
-data = pd.DataFrame(index=range(0,len(df)),columns=['DATE','Downtime (hrs)'])
+df2 = df2.sort_index(ascending=True,axis=0)
+data = pd.DataFrame(index=range(0,len(df2)),columns=['DATE','Downtime (hrs)'])
 
 
 
 for i in range(0,len(data)):
-    data["DATE"][i]=df['DATE'][i]
-    data["Downtime (hrs)"][i]=df["Downtime (hrs)"][i]
+    data["DATE"][i]=df2['DATE'][i]
+    data["Downtime (hrs)"][i]=df2["Downtime (hrs)"][i]
     
 data
 
@@ -197,8 +198,8 @@ scaler=MinMaxScaler(feature_range=(0,1))
 data.index=data.DATE
 data.drop('DATE',axis=1,inplace=True)
 final_data = data.values
-train_data=final_data[0:200,:]
-valid_data=final_data[200:,:]
+train_data=final_data[0:24,:]
+valid_data=final_data[24:,:]
 scaler=MinMaxScaler(feature_range=(0,1))
 scaled_data=scaler.fit_transform(final_data)
 x_train_data,y_train_data=[],[]
@@ -206,37 +207,41 @@ for i in range(60,len(train_data)):
     x_train_data.append(scaled_data[i-60:i,0])
     y_train_data.append(scaled_data[i,0])
 
+x_train_data
+y_train_data
 
 #Long Short-Term Memory Model
 lstm_model=Sequential()
-lstm_model.add(LSTM(units=50,return_sequences=True,input_shape=(np.shape(x_train_data)[1],1)))
+lstm_model.add(LSTM(units=1,return_sequences=True,input_shape=(np.shape(x_train_data)[0],1)))
 lstm_model.add(LSTM(units=50))
 lstm_model.add(Dense(1))
-model_data=data[len(data)-len(valid_data)-60:].values
+model_data=data[len(data)-len(valid_data)-20:].values
 model_data=model_data.reshape(-1,1)
 model_data=scaler.transform(model_data)
 
+
 #This step covers the preparation of the train data and the test data
 lstm_model.compile(loss='mean_squared_error',optimizer='adam')
-#lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
+lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
 X_test=[]
-for i in range(60,model_data.shape[0]):
-    X_test.append(model_data[i-60:i,0])
+for i in range(2,model_data.shape[0]):
+    X_test.append(model_data[i-2:i,0])
 X_test=np.array(X_test)
 #X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
 
 
 
 #In this step, we are running the model using the test data we defined in the previous step
-#predicted_stock_price=lstm_model.predict(X_test)
-#predicted_stock_price=scaler.inverse_transform(predicted_stock_price)
+predicted_stock_price=lstm_model.predict(X_test)
+predicted_stock_price=scaler.inverse_transform(predicted_stock_price)
 
 
 #Prediction results
 
 train_data=data[:200]
 valid_data=data[200:]
-#valid_data['Predictions']=predicted_stock_price
-#plt.plot(train_data["Close"])
-#plt.plot(valid_data[['Close',"Predictions"]])
+valid_data['Predictions']=predicted_stock_price
+plt.plot(train_data["Close"])
+plt.plot(valid_data[['Close',"Predictions"]])
+
 
